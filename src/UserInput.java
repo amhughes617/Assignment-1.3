665a36bc8cda5cd2985aec69e958664b30ad23b8
Alex <amhughes@g.cofc.edu>
2016-02-03 14:34:26 -0500

    converted int balance to a double

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 /**
  * Created by alexanderhughes on 2/3/16.
  */
@@ -5,6 +7,7 @@ public class UserInput {        //this class holds methods that handle user inpu
     String name;
     String howMuch;
     String selection;
+    double balance = 100.00;
 
     public void enterName() throws Exception {
         System.out.println("Please enter your name");
@@ -24,16 +27,18 @@ public class UserInput {        //this class holds methods that handle user inpu
         selection = Atm.scanner.nextLine();
 
         if(selection.equals("1")) {             //prints the user's balance
-            System.out.println("Your balance is $100.");
+            System.out.println("Your balance is $" + balance);
         }
         else if(selection.equals("2")) {        //allows user to withdraw
             System.out.println("How much much money would you like to withdraw?");
             howMuch = Atm.scanner.nextLine();
-            int howMuchInt = Integer.valueOf(howMuch);  //converts money input howMuch from string to int
-            if(howMuchInt > 100) {
+            double howMuchDouble = Double.valueOf(howMuch);  //converts money input howMuch from string to int
+            if(howMuchDouble > balance) {        //float is bad for money
                 throw new Exception("Insufficient funds!");
             }
+            balance -= howMuchDouble;
             System.out.println("Please take your money.");
+            System.out.println("Remaining balance = $" + String.format("%.2f", balance));  //forces it to print 2 decimal places for balance
         }
         else if(selection.equals("3")) {            //cancels transaction
             System.out.println("Thank you please come again.");
